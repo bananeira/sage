48d091614ed2f66be16206a502efb11c671b5930
bananeira <ijamil.osoria@gmail.com>
2022-04-22 21:33:23 +0200

    translated placeholder to component

---
+++ src/app/content-placeholder/content-placeholder.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-placeholder',
+  templateUrl: './content-placeholder.component.html',
+  styleUrls: ['./content-placeholder.component.scss']
+})
+export class ContentPlaceholderComponent {
+
+}
