586139732874e6df44ce8eb666f1f2be4c387d37
bananeira <ijamil.osoria@gmail.com>
2022-04-23 15:58:45 +0200

    fixed style, removed link

--- src/app/constants/section-content.constant.ts
+++ src/app/constants/section-content.constant.ts
@@ -4,9 +4,9 @@ export const textSectionContent: SectionContentModel[] = [
     {
         id: "about",
         title: "about me",
-        text: "hello! i'm jamil osoria peralta. i'm a 17-year-old student from hamburg, germany. i am currently particularly " +
-            "interested in web application development and, somewhat off-topic, playing the piano. at the moment i'm in my final " +
-            "exam phase. i plan to study software systems development afterwards. furthermore, i'm very much looking " +
+        text: "hello! i'm jamil osoria peralta. i'm a 17-year-old student from hamburg, germany. currently my interests " +
+            "especially focus on web application development and, somewhat off-topic, playing the piano. at the moment i'm in my final " +
+            "exam phase. afterwards, my plan is to study software systems development. furthermore, i'm very much looking " +
             "forward to future projects and learning a lot of new things in the process.",
     },
     {
