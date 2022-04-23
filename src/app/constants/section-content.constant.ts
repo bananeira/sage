3ab45c4d9cdf8c170a6f5e926bd50e7724b7aaf3
bananeira <ijamil.osoria@gmail.com>
2022-04-23 14:45:44 +0200

    fixed grammar

--- src/app/constants/section-content.constant.ts
+++ src/app/constants/section-content.constant.ts
@@ -6,7 +6,7 @@ export const textSectionContent: SectionContentModel[] = [
         title: "about me",
         text: "hello! i'm jamil osoria peralta. i'm a 17-year-old student from hamburg, germany. i am currently particularly " +
             "interested in web application development and, somewhat off-topic, playing the piano. at the moment i'm in my final " +
-            "exam phase and plan to study software systems development afterwards. furthermore, i'm very much looking " +
+            "exam phase. i plan to study software systems development afterwards. furthermore, i'm very much looking " +
             "forward to future projects and learning a lot of new things in the process.",
     },
     {
