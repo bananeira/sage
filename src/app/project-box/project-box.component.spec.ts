769242fed44bcfa22538ace5879c5a862a7d6bdb
bananeira <ijamil.osoria@gmail.com>
2022-04-24 01:07:32 +0200

    prepared more content components

---
+++ src/app/project-box/project-box.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectBoxComponent } from './project-box.component';
+
+describe('ProjectBoxComponent', () => {
+  let component: ProjectBoxComponent;
+  let fixture: ComponentFixture<ProjectBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectBoxComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/skill-box/skill-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-box',
+  templateUrl: './skill-box.component.html',
+  styleUrls: ['./skill-box.component.scss']
+})
+export class SkillBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
