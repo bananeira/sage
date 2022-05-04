dfaf53c7632d9bbddf54e479a24eb13e08c9a1c2
bananeira <ijamil.osoria@gmail.com>
2022-05-04 14:37:26 +0200

    edited about me text

--- src/app/constants/section-content.constant.ts
+++ src/app/constants/section-content.constant.ts
@@ -10,7 +10,7 @@ export const sectionContentModels: SectionContentModel[] = [
         title: "about me",
         text: "Hello! I'm Jamil Osoria Peralta. I'm a 17-year-old student. Currently my interests " +
             "focus particularly on web application development and, somewhat off-topic, playing the piano. " +
-            "At the moment I'm in my finals, but afterwards my plan is to study software systems development. " +
+            "At the moment I'm in my finals, afterwards my plan is to study software systems development. " +
             "Furthermore, I'm very much looking forward to future projects and learning a lot of new " +
             "things in the process.",
     },
