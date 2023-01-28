40824221ddca8b8bed188795e22433d7eaa26221
sora <ijamil.osoria@gmail.co>
2023-01-28 14:41:26 +0100

    corrects responsiveness

--- src/app/complement-builder-showcase/complement-builder-showcase.component.ts
+++ src/app/complement-builder-showcase/complement-builder-showcase.component.ts
@@ -98,12 +98,12 @@ export class ComplementBuilderShowcaseComponent {
             (please note that this is a non-formal concept, which applies to any arbitrary radix):
             \\begin{align}
             \\end{align}
-            $S_{10} \\rightarrow \\overline{S_{10}}:$
-            \\begin{array}{|c|cccccccccc|}
+            $S_{10} \\text{ (top row)} $ $\\rightarrow \\overline{S_{10}} \\text{ (bottom row)}:$
+            \\begin{array}{|cccccccccc|}
                 \\hline
-                S_{10} & 0 & 1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9\\\\
+                0 & 1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9\\\\
                 \\hline
-                \\overline{S_{10}} & 9 & 8 & 7 & 6 & 5 & 4 & 3 & 2 & 1 & 0\\\\
+                9 & 8 & 7 & 6 & 5 & 4 & 3 & 2 & 1 & 0\\\\
                 \\hline
             \\end{array}
             \\begin{align}
