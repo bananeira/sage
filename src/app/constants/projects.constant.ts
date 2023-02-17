3d3394e1eed11e15f5c4077549fde20d9647ea94
Sora <ijamil.osoria@gmail.com>
2023-02-17 22:57:43 +0100

    Update projects.constant.ts

--- src/app/constants/projects.constant.ts
+++ src/app/constants/projects.constant.ts
@@ -3,18 +3,29 @@ import {State} from "../enum/state.enum";
 
 export const projects: LinkModel[] = [
     {
-        id: "complementbuilder",
-        title: "complement builder",
-        description: "The complement representation of numbers is used by computers to " +
-            "make operations like subtraction more efficient. This tool explains and calculates " +
-            "the complement of numbers to an arbitrary denominational number system (radix 2-16).",
+        id: "sage-toolbox",
+        title: "sage-toolbox",
+        description: "This is a small Java project in Spring. Only the Complement Builder" +
+            " is included yet. More may follow later.",
         language: [
             "java",
+            "spring",
         ],
         remit: [
 
         ],
-        codeLink: "https://github.com/bananeira/sage-toolbox/blob/main/src/main/java/com/sage/sagetoolbox/service/ComplementBuilder.java",
+        codeLink: "https://github.com/bananeira/sage-toolbox",
+        state: State.Concluded
+    },
+    {
+        id: "recursion",
+        title: "recursion",
+        description: "A scientific derivation on my part to explain the principle of recursion in an understandable way.",
+        language: [
+        ],
+        remit: [
+        ],
+        codeLink: "https://me.sorata.dev#projects",
         state: State.Concluded
     },
     {
