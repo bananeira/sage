d9e1a5f02e743df031f4a699fd7dcf415bc24084
sora <ijamil.osoria@gmail.com>
2023-03-05 00:43:32 +0100

    shorten transition

--- src/app/section-content/section-content.component.ts
+++ src/app/section-content/section-content.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
 import {SectionContentModel} from "../interface/section-content-model";
 import {ToolShowcaseModel} from "../interface/tool-showcase-model";
 import {tools} from "../constants/tools.constant";
@@ -19,4 +19,21 @@ export class SectionContentComponent {
     rsaTool = tools.find(
         (toolShowcaseModel?: ToolShowcaseModel) => (toolShowcaseModel?.id === "rsa")
     );
+
+    @HostListener('window:scroll', ['$event']) // for window scroll events
+    onScroll(element: { getBoundingClientRect: () => any; }, threshold: number, mode: string) {
+        this.isOnScreen(element, threshold, mode);
+    }
+
+    isOnScreen(elm: { getBoundingClientRect: () => any; }, threshold: number, mode?: string) {
+        threshold = threshold || 0;
+        mode = mode || 'visible';
+
+        const rect = elm.getBoundingClientRect();
+        const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+        const above = rect.bottom - threshold < 0;
+        const below = rect.top - viewHeight + threshold >= 0;
+
+        return mode === 'above' ? above : (mode === 'below' ? below : !above && !below);
+    }
 }
