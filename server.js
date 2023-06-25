5a7954b03ee0391d3233087d5714cdc7d4433c9f
bananeira <ijamil.osoria@gmail.com>
2023-06-25 02:14:19 +0200

    Try to fix ssl redirect in server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var sslRedirect = require('heroku-ssl-redirect');
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
-app.use(sslRedirect());
 
 const express = require('express');
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/sage'));
+app.use(express.static(__dirname + '/dist/sage'), sslRedirect());
 app.get('/*', function(req,res) {
         res.sendFile(path.join(__dirname+'/dist/sage/index.html'));
     });
