7801720f7c029a93979bf21e47e2a14d3bd4eade
bananeira <ijamil.osoria@gmail.com>
2023-06-26 22:12:51 +0200

    Try undestroying more server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-
 const express = require('express');
 const path = require('path');
-const {listen} = require("express/lib/application");
-const {use} = require("express/lib/router");
-const {get} = require("karma/lib/server");
+const app = express();
 
-use(express.static(__dirname + '/dist/sage'));
-get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname+'/dist/sage/index.html'));
-    });
-
-listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/sage'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/sage/index.html'));
+});
+app.listen(process.env.PORT || 8080);
