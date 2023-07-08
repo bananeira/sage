3fe5509bacf2f258f7b21de968745562e5e47bcb
bananeira <ijamil.osoria@gmail.com>
2023-07-08 18:25:20 +0200

    Check if http

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const path = require('path');
 var enforce = require('express-sslify');
 const app = express();
 
-app.use(enforce.HTTPS(), express.static(__dirname + '/dist/sage'));
+if (location.protocol !== 'https:') {
+    app.use(enforce.HTTPS());
+}
+
+app.use(express.static(__dirname + '/dist/sage'));
+
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/sage/index.html'));
 });
