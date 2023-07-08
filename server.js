d9fb559649da75db0fac004e207ea0b2f6a4e384
bananeira <ijamil.osoria@gmail.com>
2023-07-08 18:46:29 +0200

    Apparently we dont like enforcing https

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-var enforce = require('express-sslify');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/sage'));
 
-if (location.protocol !== 'https:'){
-    app.use(enforce.HTTPS());
-}
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/sage/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
