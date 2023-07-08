67cb523f123b4ac7e32b3d1202d206d8140c816b
bananeira <ijamil.osoria@gmail.com>
2023-07-08 03:24:09 +0200

    Try implement express-sslify

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+var enforce = require('express-sslify');
+const http = require("http");
 const app = express();
 
-app.use(express.static(__dirname + '/dist/sage'));
+app.use(express.static(__dirname + '/dist/sage'), enforce.HTTPS());
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/sage/index.html'));
 });
